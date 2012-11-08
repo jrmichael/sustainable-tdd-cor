8643aef92353bb1370bdca477585723ed1c38674
Michal Bareja <michael.bareja@gmail.com>
2012-11-08 13:01:57 +0100

    fixed

--- src/test/java/cor/processors/ProcessorFactoryTest.java
+++ src/test/java/cor/processors/ProcessorFactoryTest.java
@@ -20,22 +20,10 @@ public class ProcessorFactoryTest {
 
     @Test
     public void FactoryReturnsProperChainOfProcessors() {
-    	ProcessorFactory mockedFactory = mock(ProcessorFactory.class);
-    	TerminalProcessor terminalProcessor = new TerminalProcessor();
-    	SmallValueProcessor smallValueProcessor = new SmallValueProcessor(terminalProcessor);
-    	LargeValueProcessor largeValueProcessor = new LargeValueProcessor(smallValueProcessor);
-		when(mockedFactory.terminalProcessor()).thenReturn(terminalProcessor);
-		when(mockedFactory.smallValueProcessor(any(Processor.class))).thenReturn(smallValueProcessor);
-		when(mockedFactory.largeValueProcessor(any(Processor.class))).thenReturn(largeValueProcessor);
-		when(mockedFactory.getProcessor()).thenCallRealMethod();
-		
-		Processor processor = mockedFactory.getProcessor();
-		
-		verify(mockedFactory).terminalProcessor();
-		verify(mockedFactory).smallValueProcessor(terminalProcessor);
-		verify(mockedFactory).largeValueProcessor(smallValueProcessor);
-    	assertThat(processor).isEqualTo(largeValueProcessor);
-    	
+		Processor processor = factory.getProcessor();
+		assertThat(processor).isInstanceOf(LargeValueProcessor.class);
+		assertThat(processor.delegate).isInstanceOf(SmallValueProcessor.class);
+		assertThat(processor.delegate.delegate).isInstanceOf(TerminalProcessor.class);
     }
 
 }
